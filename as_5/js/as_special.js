5b375f54c61ce4f015070c51ea5f7c1a53f14b07
taijingyan <38680568+taijingyan@users.noreply.github.com>
2021-12-14 10:33:35 +0800

    upload image

--- as_5/js/as_special.js
+++ as_5/js/as_special.js
@@ -11,3 +11,40 @@ $(document).ready(function () {
 		});
 	});
 });
+
+$(window).scroll(function () {
+	var scrolled = $(document).scrollTop();
+	var winh = $(window).height();
+	$(".opa").each(function () {
+		var opRange = $(this).parent().height() / 5;
+		$(this).css({
+			opacity: ($(this).data("seq") * opRange - scrolled) / opRange
+		});
+	});
+
+	$(".maploc").each(function () {
+		var thispos = $(this).offset().top;
+		var thish = $(this).height();
+		if (scrolled >= thispos && scrolled < thispos + thish) {
+			$("#bg-img").css({
+				transform:
+					"translate(" +
+					$(this).data("x") +
+					", " +
+					$(this).data("y") +
+					") scale(" +
+					$(this).data("scale") +
+					")"
+			});
+		}
+	});
+	if (
+		scrolled < $("#mapmove").offset().top ||
+		scrolled > $("#mapmove").offset().top + $("#mapmove").height()
+	) {
+		$("#bg-img").css({
+			transform: "translate(0%, 0%) scale(1)"
+		});
+	}
+});
+
